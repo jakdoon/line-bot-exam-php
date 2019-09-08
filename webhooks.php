b41467156b4ac189172eed5077db9d0f0ce70357
jakdoon <44129959+jakdoon@users.noreply.github.com>
2019-09-08 13:21:30 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -46,7 +46,6 @@ $jsonFlex = [ //1
             "type" => "text",
             "text" => "Avenue ยินดีต้อนรับค่ะ",
             "size" => "xxl",
-            "offsetTop" => "15px",
             "weight" => "bold",
             "color" => "#000000"
           ],
